a52bb28a1eb903f48bc3c1d5b8b1416895329ae3
Anshnoor Kaur <anshnoorkaur100@gmail.com>
2024-04-11 12:33:50 -0700

    Added vignette and made changes to histogram tests

--- tests/testthat/test-histograms.R
+++ tests/testthat/test-histograms.R
@@ -10,31 +10,31 @@ test_that("data_frame should not be empty",
 })
 
 test_that("xlab should be a string", 
-{expect_error(make_histogram(list_wrong_type, "num_imgs", "shares", 
+{expect_error(make_histogram(good_df, "num_imgs", "shares", 
 "Histogram of shares against number of images", num_imgs))
 })
 
 test_that("title should be a string", 
-{expect_error(make_histogram(list_wrong_type, "num_imgs", "shares", 
+{expect_error(make_histogram(good_df, "num_imgs", "shares", 
 shares, "Number of Images"))
 })
 
 test_that("num_imgs should be a string", 
-{expect_error(make_histogram(list_wrong_type, num_imgs, "shares", 
+{expect_error(make_histogram(good_df, num_imgs, "shares", 
 shares, "Number of Images"))
 })
 
 test_that("shares should be a string", 
-{expect_error(make_histogram(list_wrong_type, "num_imgs", shares, 
+{expect_error(make_histogram(good_df, "num_imgs", shares, 
 shares, "Number of Images"))
 })
 
 test_that("xvar should be a valid column in data_frame", 
-{expect_error(make_histogram(list_wrong_type, "num_images", "shares", 
+{expect_error(make_histogram(good_df, "num_images", "shares", 
 "Histogram of shares against number of images", "Number of Images"))
 })
 
 test_that("yvar should be a valid column in data_frame", 
-{expect_error(make_histogram(list_wrong_type, "num_imgs", "num_images", 
+{expect_error(make_histogram(good_df, "num_imgs", "num_images", 
 "Histogram of shares against number of images", "Number of Images"))
 })
